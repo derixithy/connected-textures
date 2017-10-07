e2f3b6830c3ada7b13d97d34e52f4aa217c90f5d
Derixithy <derixithy@users.noreply.github.com>
2017-10-07 16:51:52 +0200

    Create init.lua
    Replace tiles for dirt blocks

---
+++ init.lua
@@ -0,0 +1,21 @@
+
+minetest.override_item('default:dirt_with_grass', {
+	tiles = {"default_grass.png", "default_dirt.png",
+		{name = "default_grass.png",
+			tileable_vertical = false}},
+})
+minetest.override_item('default:dirt_with_dry_grass', {
+	tiles = {"default_dry_grass.png", "default_dirt.png",
+		{name = "default_dry_grass.png",
+			tileable_vertical = false}},
+})
+minetest.override_item('default:dirt_with_snow', {
+	tiles = {"default_snow.png", "default_dirt.png",
+		{name = "default_snow.png",
+			tileable_vertical = false}},
+})
+minetest.override_item('default:dirt_with_rainforest_litter', {
+	tiles = {"default_rainforest_litter.png", "default_dirt.png",
+		{name = "default_rainforest_litter.png",
+			tileable_vertical = false}},
+})
